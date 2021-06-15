10ed20b80efb668837b4eba1f87c2d7b9ac44df6
Marvelxy <still4marvelous@gmail.com>
2021-06-15 02:19:20 +0100

    Finish up task 5.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -95,7 +95,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('page1'));
+        return new RedirectResponse($this->urlGenerator->generate('index'));
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
